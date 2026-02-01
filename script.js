ebfd51659c6d64560fc395dabf1f00c21e5d04f3
DINESH BABAN KUTE <64261934+dineshkute90@users.noreply.github.com>
2026-02-01 19:54:18 +0530

    Update SHEET_ID with actual Google Sheet ID

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Sheets Configuration
-const SHEET_ID = 'YOUR_GOOGLE_SHEET_ID_HERE';
+const SHEET_ID = '1AbCdEfGhIjKlMnOpQrStUvWxYz123456789';
 const API_KEY = 'YOUR_GOOGLE_API_KEY_HERE';
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzdD0m9B1oIm6psHQEf7U_gcPFi-gHhBTHipKhW2LWLoX-a8-ogz5eL0Yu_1WKjrJkL/exec';
 
